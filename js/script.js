d4df3479dcb57fa3c40dcbf8b0758b47403b854d
Mettralla <daxter0493@gmail.com>
2022-05-04 23:05:00 -0300

    refactor script to integrate ui

--- js/script.js
+++ js/script.js
@@ -1,75 +1,98 @@
 function computerPlay() {
     const option = Math.floor(Math.random() * 3) + 1;
-    let computerSelection = "null";
+    let computerSelection = null;
+    let pcHand = document.getElementById('pcHand');
     if (option == 1) {
-        computerSelection = "rock";
+        pcHand.src= "./images/rockPc.svg";
+        pcHand.alt="Rock"
+        computerSelection = "Rock"
     } else if (option == 2) {
-        computerSelection = "paper";
+        pcHand.src= "./images/paperPc.svg"
+        pcHand.alt="Paper"
+        computerSelection = "Paper"
     } else {
-        computerSelection = "scissors";
+        pcHand.src="./images/scissorsPc.svg";
+        pcHand.alt="Scissors"
+        computerSelection = "Scissors"
     }
     return computerSelection;
 }
 
+function playerPlay(playerSelection) {
+    let playerHand = document.getElementById('playerHand');
+    if (playerSelection == "Rock") {
+        playerHand.src= "./images/rockUser.svg";
+        playerHand.alt="Rock";
+    } else if (playerSelection == "Paper") {
+        playerHand.src= "./images/paperUser.svg";
+        playerHand.alt="Paper";
+    } else {
+        playerHand.src= "./images/scissorsUser.svg";
+        playerHand.alt="Scissors";
+    }
+    return playerSelection;
+}
+
+function reset() {
+    playerVictory = 0;
+    computerVictory = 0;
+}
+
+function conditionChecker(result) {
+    let score = document.getElementById('scorePoints');
+    let message = document.getElementById('message');
+    if (playerVictory != 5 && computerVictory != 5) {
+        message.textContent = result;
+        score.textContent = `Player Score ${playerVictory} - ${computerVictory} Machine Score`
+    } else if (playerVictory == 5) {
+        message.style.color = 'green';
+        message.textContent = `If you want to play again, choose your hand!`;
+        score.style.color = 'green';
+        score.textContent = `Congratulations! You've won the match with ${playerVictory} points!`;
+        reset();
+    } else {
+        message.style.color = 'red';
+        message.textContent = `If you want a rematch, choose your hand!`;
+        score.style.color = 'red';
+        score.textContent = `Sorry, you lose! Computer have won the match with ${computerVictory} points!`;
+        reset();
+    }
+}
+
 function playRound(computerSelection, playerSelection) {
-    let lowerPlayer = playerSelection.toLowerCase();
-    console.log(`Computer choose: ${computerSelection}`);
-    console.log(`You choose: ${lowerPlayer}`);
     let result = 0;
-    if (computerSelection === lowerPlayer) {
-        result = `Draw! Both choose ${lowerPlayer}`;
-    } else if (computerSelection == "rock" && lowerPlayer == "paper") {
-        result = `You Win! ${lowerPlayer} beats ${computerSelection}`;
-    } else if (computerSelection == "paper" && lowerPlayer == "rock") {
-        result = `You Lose! ${computerSelection} beats ${lowerPlayer}`;
-    } else if (computerSelection == "paper" && lowerPlayer == "scissors") {
-        result = `You Win! ${lowerPlayer} beats ${computerSelection}`;
-    } else if (computerSelection == "scissors" && lowerPlayer == "paper") {
-        result = `You Lose! ${computerSelection} beats ${lowerPlayer}`;
-    } else if (computerSelection == "scissors" && lowerPlayer == "rock") {
-        result = `You Win! ${lowerPlayer} beats ${computerSelection}`;
-    } else if (computerSelection == "rock" && lowerPlayer == "scissors") {
-        result = `You Lose! ${computerSelection} beats ${lowerPlayer}`;
+    if (computerSelection === playerSelection) {
+        result = `Draw! Both choose ${playerSelection}`;
+    } else if (computerSelection == "Rock" && playerSelection == "Paper") {
+        result = `You Won! ${playerSelection} beats ${computerSelection}`;
+        playerVictory += 1;
+    } else if (computerSelection == "Paper" && playerSelection == "Rock") {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerVictory += 1;
+    } else if (computerSelection == "Paper" && playerSelection == "Scissors") {
+        result = `You Won! ${playerSelection} beats ${computerSelection}`;
+        playerVictory += 1;
+    } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerVictory += 1;
+    } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
+        result = `You Won! ${playerSelection} beats ${computerSelection}`;
+        playerVictory += 1;
+    } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerVictory += 1;
     }
-    return result;
+    conditionChecker(result);
 }
 
-function game(){
-    //ESTA FUNCION REALIZA 5 RONDAS DEL JUEGO Y DEVUELVE EL RESULTADO FINAL
-    //ESTOS CONTADORES LLEVAN EL RESULTADO DE LAS 5 RONDAS
-    let playerVictory = 0;
-    let computerVictory = 0;
-    let finalScore = 0;
-    //BUCLE DE 5 RONDAS
-    for(let i = 0; i < 5; i++) {
-        //ESTA FUNCION DEVUELVE LA MANO DE LA COMPUTADORA
-        let computerSelection = computerPlay();
-        //ESTA VARIABLE ES LA MANO DEL JUGADOR
-        let playerSelection = prompt("Choose your hand: ");
-        //ESTA VARIABLE DEVUELVE EL RESULTADO DE LA RONDA
-        let matchResult = playRound(computerSelection, playerSelection);
-        //MOSTRAR GANADOR DE LA RONDA
-        console.log(matchResult);
-        //DETECTA SI GANASTE USANDO SLIDING Y AUMENTA LOS CONTADORES
-        let short = matchResult.slice(4,7);
-        if (short == `Win`) {
-            playerVictory += 1;
-        } else if (short == `Los`) {
-            computerVictory +=1;
-        }
-    }
-    //DETECTA QUIEN GANO EL JUEGO
-    if (playerVictory > computerVictory) {
-        finalScore = `Congratulations! You won with ${playerVictory} against ${computerVictory}`;
-        return finalScore;
-    } else if (playerVictory < computerVictory) {
-        finalScore = `Sorry! You lose with ${playerVictory} against ${computerVictory}`;
-        return finalScore;
-    } else if (playerVictory == computerVictory) {
-        finalScore = `It's a Draw! Both have ${playerVictory}`;
-        return finalScore;
-    }
-}
+let playerVictory = 0;
+let computerVictory = 0;
+let finalScore = 0;
 
-let result = game();
-console.log(result);
+const rockBtn = document.getElementById('btn1');
+const paperBtn = document.getElementById('btn2');
+const scissorsBtn = document.getElementById('btn3');
+
+rockBtn.addEventListener('click', () => playRound(computerPlay(), playerPlay("Rock")));
+paperBtn.addEventListener('click', () => playRound(computerPlay(), playerPlay("Paper")));
+scissorsBtn.addEventListener('click', () => playRound(computerPlay(), playerPlay("Scissors")));
